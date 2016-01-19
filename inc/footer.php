83f6d478c48bd9bd64624430cf696a5c08d5de21
Jason Righelato <jayontraps@gmail.com>
2016-01-19 17:37:28 +0000

    .gitignore is now working

--- inc/footer.php
+++
@@ -1,7 +0,0 @@
-
-	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
-	<script type="text/javascript" src="js/jquery.easing.js"></script>
-	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-	<script src="js/main.js"></script>
-
-</body>
