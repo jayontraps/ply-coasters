0305b84679017b4e6906ee7c3f352eea8128cc2f
Jason Righelato <jayontraps@gmail.com>
2018-05-17 21:32:12 +0100

    update events page

--- inc/contact.php
+++ inc/contact.php
@@ -61,63 +61,48 @@
 					<span class="event-details"></span>
 				</li>
  -->
- 				<li class="event-li">
-					<span class="event-date">Sunday 19th Nov 2017 - 10.30am – 4.45pm </span>
-					<span class="event-link">
-						<a href="http://www.friends-of-age-exchange.org.uk/index.php?page_id=56" target="_blank">Blackheath Christmas Fair</a>
-					</span>
-					<span class="event-details">Blackheath Halls</span>
-				</li>
 
- 				<li class="event-li">
-					<span class="event-date">Saturday 25th Nov 2017 - 12 – 4pm </span>
-					<span class="event-link">
-						<a href="http://www.holyspirit-clapham.org.uk/news/christmasfair/christmas-fair" target="_blank">Church of The Holy Spirit Christmas Fair</a>
-					</span>
-					<span class="event-details">Norbonne Avenue, Clapham SW4</span>
-				</li>
+				 <li class="event-li">
+				 	<span class="event-date">Sunday 20th May 11am–7pm </span>
+				 	<span class="event-link">
+				 		<a href="http://greenwichpeninsula.co.uk/whats-on/urban-village-fete-2018/" target="_blank">Urban Village Fete</a>
+				 	</span>
+				 	<span class="event-details">Greenwich Peninsula</span>
+				 </li>
 
- 				<li class="event-li">
-					<span class="event-date">Sunday 26th Nov 2017 - 11.30am – 4pm </span>
-					<span class="event-link">
-						<a href="http://www.friendsofdulwichcollege.co.uk/" target="_blank">Dulwich College Christmas Fair</a>
-					</span>
-					<span class="event-details">Dulwich College</span>
-				</li>
+				 <li class="event-li">
+				 	<span class="event-date">Saturday 16th June</span>
+				 	<span class="event-link">
+				 		<a href="https://crystalpalacefestival.org/locations/" target="_blank">Crystal Palace Festival</a>
+				 	</span>
+				 	<span class="event-details">Crystal Palace Park</span>
+				 </li>
 
- 				<li class="event-li">
-					<span class="event-date">Saturday 2nd December 2017 10am – 4pm </span>
-					<span class="event-link">
-						Ladywell Christmas Market
-					</span>
-					<span class="event-details">Ladywell Road, London SE13</span>
-				</li>
-
- 				<li class="event-li">
-					<span class="event-date">Saturday 3rd December 2017 11am – 3pm </span>
-					<span class="event-link">
-						<a href="https://www.morleycollege.ac.uk/events/1288-morley-winter-fair" target="_blank">Morley College Winter Fair</a>
-					</span>
-					<span class="event-details">61 Westminster Bridge Road, London SE1 7HT</span>
-				</li>
-
- 				<li class="event-li">
-					<span class="event-date">hursday 7th December 2017 - 5.30pm – 9.30pm </span>
-					<span class="event-link">
-						<a href="http://www.horniman.ac.uk/visit/events/horniman-christmas-market" target="_blank">Horniman Museum Christmas Market</a>
-					</span>
-					<span class="event-details">100 London Rd, Forest Hill, London SE23 3PQ</span>
-				</li>
+				 <li class="event-li">
+					 <span class="event-date">Sunday 17th June - 10am - 4pm</span>
+					 <span class="event-link">
+						 Heritage Craft Market at the Crystal Palace Subway
+					 </span>
+					 <span class="event-details">Near Bus stop B, Crystal Palace Parade</span>
+				 </li>
 
 
- 				<li class="event-li">
-					<span class="event-date">Saturday 9th December 2107 - 12pm – 5pm </span>
-					<span class="event-link">
-						<a href="http://www.camberwellarts.org.uk/whats-on/2017/12/9/dqdgxmfqr91yk4sjqfvjlnxou47eb9" target="_blank">Made in Camberwell Arts Fair</a>
-					</span>
-					<span class="event-details">The Tiger, 18 Camberwell Grn London SE5 7AA</span>
-				</li>
+				 <li class="event-li">
+				 	<span class="event-date">Sunday 24th June</span>
+				 	<span class="event-link">
+				 		Flea @ Flat Iron Square
+				 	</span>
+				 	<span class="event-details">53 Southwark Street, London SE1 1RU</span>
+				 </li>
 
+
+				 <li class="event-li">
+					 <span class="event-date">Saturday 30th June 12-5pm</span>
+					 <span class="event-link">
+						 <a href="http://www.abbevillefete.com" target="_blank">Abbeville Fete</a>
+					 </span>
+					 <span class="event-details">Clapham</span>
+				 </li>
 
 				</ul>
 				<div class="Button Button--contact modal">Say hello..</div>
@@ -128,11 +113,3 @@
 		</div>
 	</div>
 </div>
-
-
-
-
-
-
-
-
