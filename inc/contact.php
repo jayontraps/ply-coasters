dedaa762e791361771773a935a0c3f6aeea83bf7
Jason Righelato <jayontraps@gmail.com>
2018-11-07 22:12:19 +0000

    updated list

--- inc/contact.php
+++ inc/contact.php
@@ -62,47 +62,71 @@
 				</li>
  -->
 
+				 
+
+				 <!-- new items.. -->
 				 <li class="event-li">
-				 	<span class="event-date">Sunday 20th May 11am–7pm </span>
-				 	<span class="event-link">
-				 		<a href="http://greenwichpeninsula.co.uk/whats-on/urban-village-fete-2018/" target="_blank">Urban Village Fete</a>
-				 	</span>
-				 	<span class="event-details">Greenwich Peninsula</span>
-				 </li>
-
-				 <li class="event-li">
-				 	<span class="event-date">Saturday 16th June</span>
-				 	<span class="event-link">
-				 		<a href="https://crystalpalacefestival.org/locations/" target="_blank">Crystal Palace Festival</a>
-				 	</span>
-				 	<span class="event-details">Crystal Palace Park</span>
-				 </li>
-
-				 <li class="event-li">
-					 <span class="event-date">Sunday 17th June - 10am - 4pm</span>
-					 <span class="event-link">
-						 Heritage Craft Market at the Crystal Palace Subway
-					 </span>
-					 <span class="event-details">Near Bus stop B, Crystal Palace Parade</span>
-				 </li>
+ 					<span class="event-date">Sunday 18th November 10.30am–4.45pm</span>
+ 					<span class="event-link">
+ 						<a href="http://www.age-exchange.org.uk/get-involved/friends-of-age-exchange/blackheath-christmas-fair/" target="_blank">Blackheath Christmas Fair</a>
+ 					</span>
+ 					<span class="event-details">Blackheath Halls</span>
+ 				</li>
 
 
-				 <li class="event-li">
-				 	<span class="event-date">Sunday 24th June</span>
-				 	<span class="event-link">
-				 		Flea @ Flat Iron Square
-				 	</span>
-				 	<span class="event-details">53 Southwark Street, London SE1 1RU</span>
-				 </li>
+				<li class="event-li">
+					<span class="event-date">Saturday 24th November 12-5pm</span>
+					<span class="event-link">
+						<a href="https://www.orleanshousegallery.org" target="_blank">Orleans House Shopping Weekend</a>
+					</span>
+					<span class="event-details">Orleans House Gallery, Riverside, Twickenham TW1 3DJ</span>
+				</li>
 
 
-				 <li class="event-li">
-					 <span class="event-date">Saturday 30th June 12-5pm</span>
-					 <span class="event-link">
-						 <a href="http://www.abbevillefete.com" target="_blank">Abbeville Fete</a>
-					 </span>
-					 <span class="event-details">Clapham</span>
-				 </li>
+				<li class="event-li">
+					<span class="event-date">Sunday 25th November 11.30–4pm</span>
+					<span class="event-link">
+						<a href="http://www.friendsofdulwichcollege.co.uk" target="_blank">Dulwich College Christmas Fair</a>
+					</span>
+					<span class="event-details">Dulwich Common SE21 7LD</span>
+				</li>
+
+
+				<li class="event-li">
+					<span class="event-date">Sunday 2nd December 11am–3pm</span>
+					<span class="event-link">
+						<a href="https://www.morleycollege.ac.uk/events/1288-morley-winter-fair" target="_blank">Morley College Winter Fair</a>
+					</span>
+					<span class="event-details">61 Westminster Bridge Road SE1 7HT</span>
+				</li>
+
+				<li class="event-li">
+					<span class="event-date">Saturday 8th December 11am-6pm</span>
+					<span class="event-link">
+						<a href="https://www.pexmas.com/about-pexmas-2018/" target="_blank">Pexmas Festive Market</a>
+					</span>
+					<span class="event-details">Warwick Gardens, Lyndhurst Way, Peckham SE15 5AQ</span>
+				</li>
+
+
+				<li class="event-li">
+					<span class="event-date">Saturday 8th December 12–6pm</span>
+					<span class="event-link">
+						<a href="https://www.facebook.com/events/272700780047173/" target="_blank">Brockley Christmas Market,</a>
+					</span>
+					<span class="event-details">Coulgate Street next to Brockley Station</span>
+				</li>
+
+
+				<li class="event-li">
+					<span class="event-date">Sunday 9th December 11am–4pm</span>
+					<span class="event-link">
+						<a href="https://www.sccoop.org.uk/events" target="_blank">The Rookery Christmas Fair</a>
+					</span>
+					<span class="event-details">Streatham Common</span>
+				</li>
+
+
 
 				</ul>
 				<div class="Button Button--contact modal">Say hello..</div>
